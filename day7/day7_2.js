5ab06fbb9d94f0fe213ac57af49a2038e5cce120
Mike Madsen <mikewmadsen@gmail.com>
2019-12-08 21:16:43 -0800

    Start day 8

--- day7/day7_2.js
+++ day7/day7_2.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const Intcode = require('./intcode_class.js').Intcode
 
-// let codeInput = fs.readFileSync('puzzle_input.txt', 'utf8').split(',').map((n) => parseInt(n))
+let codeInput = fs.readFileSync('puzzle_input.txt', 'utf8').split(',').map((n) => parseInt(n))
 let codeInput = [3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1, 27, 26,
   27, 4, 27, 1001, 28, -1, 28, 1005, 28, 6, 99, 0, 0, 5]
 
